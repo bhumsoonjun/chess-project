a20cab10943f78b2308ad5700f7eb0605dd055e8
bhumsoonjun <83196403+bhumsoonjun@users.noreply.github.com>
2023-10-17 10:33:42 +0700

    add

--- recursive/analyser_recursive_look_ahead.py
+++ recursive/analyser_recursive_look_ahead.py
@@ -27,6 +27,7 @@ class analyser_recursive_look_ahead:
         self.num_variation = num_variation
         self.white_skill_level = white_skill_level
         self.black_skill_level = black_skill_level
+        self.send_uci_new_game = False
         self.stockfish = self.get_stockfish_for_platform(stockfish_config=stockfish_config, nnue=nnue)
 
 
@@ -49,7 +50,6 @@ class analyser_recursive_look_ahead:
     def get_eval(self, move: Dict):
         centipawn = move["Centipawn"]
         mate = move["Mate"]
-        print(move)
         if centipawn is not None:
             return centipawn
         else:
@@ -59,13 +59,13 @@ class analyser_recursive_look_ahead:
                 return 20 * 100
 
     def look_ahead_eval(self, fen, move: str, is_white_move: bool):
-        self.stockfish.set_fen_position(fen, send_ucinewgame_token=True)
+        self.stockfish.set_fen_position(fen, send_ucinewgame_token=self.send_uci_new_game)
         self.stockfish.make_moves_from_current_position([move])
         top_moves = self.stockfish.get_top_moves(self.num_variation + 2)
         evaluation = 0
 
         if len(top_moves) == 0:
-            return 20 if is_white_move else -20
+            return -20 if is_white_move else 20
 
         for move in top_moves:
             evaluation += self.get_eval(move)
@@ -76,23 +76,23 @@ class analyser_recursive_look_ahead:
         look_ahead_eval = self.look_ahead_eval(fen, move, is_white_move)
         print(f"look: {look_ahead_eval}")
         if is_white_move:
-            return 0.2 - 0.2 * np.exp(-0.2 * (look_ahead_eval + 4))
+            return 0.3 - 0.3 * np.exp(-0.2 * (look_ahead_eval + 4))
         else:
-            return 0.2 - 0.2 * np.exp(0.2 * (-look_ahead_eval - 4))
+            return 0.3 - 0.3 * np.exp(0.2 * (look_ahead_eval - 4))
 
     def get_first_depth_prob(self, starting_fen: str, move: str):
-        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=True)
+        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=self.send_uci_new_game)
         for i in range(1, self.stockfish_depth + 1):
             self.stockfish.set_depth(i)
             best_move = self.stockfish.get_best_move()
             print(f"best: {best_move} {move} {i}")
             if best_move == move:
-                return 0.5 * np.exp(-0.08 * i)
+                return 0.7 * np.exp(-0.08 * i)
 
         self.stockfish.set_depth(self.stockfish_depth)
-        return 0.5 * np.exp(-0.08 * (self.stockfish_depth + 1))
+        return 0.7 * np.exp(-0.08 * (self.stockfish_depth + 1))
     def get_move_weight(self, starting_fen: str, move: str):
-        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=True)
+        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=self.send_uci_new_game)
 
         for i in range(1, self.stockfish_depth + 1):
             self.stockfish.set_depth(i)
@@ -104,25 +104,39 @@ class analyser_recursive_look_ahead:
         return 6 * np.exp(-0.1 * (self.stockfish_depth + 1))
 
     def get_next_move(self, fen: str, parent: tree_node) -> Set[Tuple[str, tree_node]]:
-        temp_set = set()
-        self.stockfish.set_fen_position(fen, send_ucinewgame_token=True)
-        best_move_with_eval = self.stockfish.get_top_moves(1)[0]
-        top_moves = self.stockfish.get_top_moves(self.num_variation + 2)
         is_white_move = self.is_white_move(fen)
+        if is_white_move:
+            self.stockfish.set_skill_level(self.white_skill_level)
+        else:
+            self.stockfish.set_skill_level(self.black_skill_level)
+
+        temp_set = set()
+        self.stockfish.set_fen_position(fen, send_ucinewgame_token=self.send_uci_new_game)
+        best_move_with_eval = self.stockfish.get_top_moves(1)
+
+        if (len(best_move_with_eval) == 0):
+            evaluation = 0
+
+            if is_white_move:
+                evaluation = 20 * 100
+            else:
+                evaluation = -20 * 100
+
+            node = tree_node(evaluation, fen, [])
+            parent.add_child(node)
+            return set()
+
+        best_move_with_eval = best_move_with_eval[0]
+        top_moves = self.stockfish.get_top_moves(self.num_variation + 2)
+
         p_play_best = self.get_first_depth_prob(fen, best_move_with_eval["Move"])
-
-        print(f"first: {p_play_best}")
-
         p_play_best += self.get_look_ahead_eval_prob(fen, best_move_with_eval["Move"], is_white_move)
 
-        print(f"second: {p_play_best}")
-
-        p_play_best += self.get_best_move_prob_with_skill_level(is_white_move)
-
-        print(f"play: {p_play_best}")
+        print(f"best: {best_move_with_eval}, p_play_best: {p_play_best}")
 
         if np.random.uniform() < p_play_best:
-            self.stockfish.set_fen_position(fen, send_ucinewgame_token=True)
+            print("ACCEPTED BEST MOVE")
+            self.stockfish.set_fen_position(fen, send_ucinewgame_token=self.send_uci_new_game)
             self.stockfish.make_moves_from_current_position([best_move_with_eval["Move"]])
             new_fen = self.stockfish.get_fen_position()
             evaluation = self.get_eval(best_move_with_eval)
@@ -134,7 +148,7 @@ class analyser_recursive_look_ahead:
         top_moves_from_fen = np.random.choice(top_moves, size=min(self.num_variation, len(top_moves)), replace=False)
 
         for move in top_moves_from_fen:
-            self.stockfish.set_fen_position(fen, send_ucinewgame_token=True)
+            self.stockfish.set_fen_position(fen, send_ucinewgame_token=self.send_uci_new_game)
             self.stockfish.make_moves_from_current_position([move["Move"]])
 
             new_fen = self.stockfish.get_fen_position()
@@ -157,7 +171,8 @@ class analyser_recursive_look_ahead:
     def evaluate(self, starting_fen: str):
         frontier: Set[Tuple[str, tree_node]] = set()
 
-        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=True)
+        self.stockfish.set_fen_position(starting_fen, send_ucinewgame_token=self.send_uci_new_game)
+        print(f"STOCKFISH EVAL: {self.stockfish.get_evaluation()}")
         init_eval = -100
         self.search_tree = tree_node(init_eval, starting_fen, [])
         frontier.add((starting_fen, self.search_tree))
@@ -174,7 +189,6 @@ class analyser_recursive_look_ahead:
                 new_frontiers.update(temp_set)
                 processing_counter += 1
 
-                print(len(new_frontiers))
             depth_counter += 1
             frontier = new_frontiers
 
--- recursive/main2.py
+++ recursive/main2.py
@@ -2,12 +2,12 @@ from recursive.analyser_recursive import analyser_recursive
 from recursive.analyser_recursive_look_ahead import analyser_recursive_look_ahead
 import numpy as np
 
-fen = "3r2rk/1p1nqpp1/2p1pn1p/p7/P2PPP2/2NQ1BR1/1P3P1P/6RK w - - 5 21"
+fen = "r2q1rk1/pbb2ppp/4p3/3pn3/NP1PnP2/3B4/PB4PP/R2Q1RK1 w - - 0 17"
 stockfish_conf = {
     "Debug Log File": "",
     "Contempt": 0,
     "Min Split Depth": 0,
-    "Threads": 1, # More threads will make the engine stronger, but should be kept at less than the number of logical processors on your computer.
+    "Threads": 8, # More threads will make the engine stronger, but should be kept at less than the number of logical processors on your computer.
     "Ponder": "false",
     "Hash": 128, # Default size is 16 MB. It's recommended that you increase this value, but keep it as some power of 2. E.g., if you're fine using 2 GB of RAM, set Hash to 2048 (11th power of 2).
     "MultiPV": 1,
@@ -22,25 +22,25 @@ stockfish_conf = {
 
 np.random.seed(1)
 
-analyser = analyser_recursive(
-    depth=4,
-    stockfish_depth=15,
-    num_variation=3,
-    stockfish_config=stockfish_conf,
-    p_black_plat_best=0,
-    p_white_play_best=0,
-    nnue="true"
-)
+# analyser = analyser_recursive(
+#     depth=4,
+#     stockfish_depth=15,
+#     num_variation=3,
+#     stockfish_config=stockfish_conf,
+#     p_black_plat_best=0,
+#     p_white_play_best=0,
+#     nnue="true"
+# )
 
 np.random.seed(1)
 
 analyser_1 = analyser_recursive_look_ahead(
-    depth=10,
-    stockfish_depth=20,
+    depth=6,
+    stockfish_depth=22,
     num_variation=3,
     stockfish_config=stockfish_conf,
-    white_skill_level=15,
-    black_skill_level=15,
+    white_skill_level=20,
+    black_skill_level=20,
     nnue="true"
 )
 
